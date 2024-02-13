d7fad84c958a863f568e59039c98ab45a7e62ab2
Arnon <>
2024-02-13 23:24:36 +0200

    Fix formatting in JsonArray.cs

--- JsonArray.cs
+++ JsonArray.cs
@@ -148,6 +148,7 @@ public sealed class JsonArray {
             return "[]";
         }
 
+
         StringBuilder sb = new();
         sb.Append('[');
         foreach (var (key, value) in values) {
